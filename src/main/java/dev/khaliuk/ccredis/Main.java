cb1d8b9c460094612d5b2f926637afb0a07e7d08
Yevhenii Khaliuk <yevhenii.khaliuk@gmail.com>
2024-03-11 22:39:35 +0100

    Configure listening port

--- src/main/java/dev/khaliuk/ccredis/Main.java
+++ src/main/java/dev/khaliuk/ccredis/Main.java
@@ -5,7 +5,13 @@ import java.net.ServerSocket;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        int port = 6379;
+        int port;
+        if (args.length >= 2 && args[0].equalsIgnoreCase("--port")) {
+            port = Integer.parseInt(args[1]);
+        } else {
+            port = 6379;
+        }
+
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             // Since the tester restarts your program quite often, setting SO_REUSEADDR
             // ensures that we don't run into 'Address already in use' errors
