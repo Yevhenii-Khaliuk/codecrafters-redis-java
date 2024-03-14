10ed9f510c9b073b606303605351fdd88063b87e
Yevhenii Khaliuk <yevhenii.khaliuk@gmail.com>
2024-03-14 03:09:14 +0100

    ACKs with no commands

--- src/main/java/dev/khaliuk/ccredis/command/ReplConf.java
+++ src/main/java/dev/khaliuk/ccredis/command/ReplConf.java
@@ -3,6 +3,8 @@ package dev.khaliuk.ccredis.command;
 import dev.khaliuk.ccredis.config.ObjectFactory;
 import org.apache.commons.lang3.math.NumberUtils;
 
+import java.util.List;
+
 public class ReplConf extends AbstractHandler {
     public ReplConf(ObjectFactory objectFactory) {
         super(objectFactory);
@@ -22,6 +24,11 @@ public class ReplConf extends AbstractHandler {
                     throw new RuntimeException("Unknown parameter: " + arguments[2]);
                 }
                 break;
+            case "getack":
+                if (!"*".equals(arguments[2])) {
+                    throw new RuntimeException("Unknown parameter: " + arguments[2]);
+                }
+                return objectFactory.getProtocolSerializer().array(List.of("REPLCONF", "ACK", "0"));
             default:
                 throw new RuntimeException("Unknown parameter: " + parameter);
         }
--- src/main/java/dev/khaliuk/ccredis/replica/ReplicaRunner.java
+++ src/main/java/dev/khaliuk/ccredis/replica/ReplicaRunner.java
@@ -2,6 +2,7 @@ package dev.khaliuk.ccredis.replica;
 
 import dev.khaliuk.ccredis.command.CommandFactory;
 import dev.khaliuk.ccredis.command.Handler;
+import dev.khaliuk.ccredis.command.ReplConf;
 import dev.khaliuk.ccredis.config.ApplicationProperties;
 import dev.khaliuk.ccredis.config.ObjectFactory;
 import dev.khaliuk.ccredis.config.ReplicaProperties;
@@ -35,43 +36,7 @@ public class ReplicaRunner extends Thread {
              OutputStream outputStream = master.getOutputStream();
              DataInputStream inputStream = new DataInputStream(master.getInputStream())
         ) {
-            // Step 1: PING
-            System.out.println("Sending PING request");
-            byte[] request = protocolSerializer.array(List.of("PING"));
-            outputStream.write(request);
-            String response = protocolDeserializer.parseInput(inputStream);
-            if (!response.equalsIgnoreCase("PONG")) {
-                throw new RuntimeException("Unexpected response for PING: " + response);
-            }
-            // Step 2: REPLCONF listening-port port
-            System.out.println("Sending REPLCONF port request");
-            request = protocolSerializer.array(
-                    List.of("REPLCONF", "listening-port", String.valueOf(applicationProperties.getPort())));
-            outputStream.write(request);
-            response = protocolDeserializer.parseInput(inputStream);
-            if (!response.equalsIgnoreCase("OK")) {
-                throw new RuntimeException("Unexpected response for REPLCONF port: " + response);
-            }
-            // Step 3: REPLCONF capa psync2
-            System.out.println("Sending REPLCONF capa request");
-            request = protocolSerializer.array(List.of("REPLCONF", "capa", "psync2"));
-            outputStream.write(request);
-            response = protocolDeserializer.parseInput(inputStream);
-            if (!response.equalsIgnoreCase("OK")) {
-                throw new RuntimeException("Unexpected response for REPLCONF capa: " + response);
-            }
-            // Step 4: PSYNC ? -1
-            System.out.println("Sending PSYNC request");
-            request = protocolSerializer.array(List.of("PSYNC", "?", "-1"));
-            outputStream.write(request);
-            response = protocolDeserializer.parseInput(inputStream);
-            if (!response.startsWith("FULLRESYNC")) {
-                throw new RuntimeException("Unexpected response for PSYNC: " + response);
-            }
-
-            protocolDeserializer.parseRdbFile(inputStream);
-
-            System.out.println("Replica has started");
+            initReplica(outputStream, inputStream);
 
             while (true) {
                 String commandString = protocolDeserializer.parseInput(inputStream);
@@ -79,10 +44,55 @@ public class ReplicaRunner extends Thread {
                 String[] arguments = commandString.split(" ");
                 String command = arguments[0].toUpperCase();
                 Handler handler = commandFactory.getCommandHandler(command);
-                handler.handle(arguments);
+                byte[] response = handler.handle(arguments);
+
+                if (handler instanceof ReplConf) {
+                    outputStream.write(response);
+                    outputStream.flush();
+                }
             }
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
         }
+    }
+
+    private void initReplica(OutputStream outputStream, DataInputStream inputStream) throws IOException {
+        // Step 1: PING
+        System.out.println("Sending PING request");
+        byte[] request = protocolSerializer.array(List.of("PING"));
+        outputStream.write(request);
+        String response = protocolDeserializer.parseInput(inputStream);
+        if (!response.equalsIgnoreCase("PONG")) {
+            throw new RuntimeException("Unexpected response for PING: " + response);
+        }
+        // Step 2: REPLCONF listening-port port
+        System.out.println("Sending REPLCONF port request");
+        request = protocolSerializer.array(
+                List.of("REPLCONF", "listening-port", String.valueOf(applicationProperties.getPort())));
+        outputStream.write(request);
+        response = protocolDeserializer.parseInput(inputStream);
+        if (!response.equalsIgnoreCase("OK")) {
+            throw new RuntimeException("Unexpected response for REPLCONF port: " + response);
+        }
+        // Step 3: REPLCONF capa psync2
+        System.out.println("Sending REPLCONF capa request");
+        request = protocolSerializer.array(List.of("REPLCONF", "capa", "psync2"));
+        outputStream.write(request);
+        response = protocolDeserializer.parseInput(inputStream);
+        if (!response.equalsIgnoreCase("OK")) {
+            throw new RuntimeException("Unexpected response for REPLCONF capa: " + response);
+        }
+        // Step 4: PSYNC ? -1
+        System.out.println("Sending PSYNC request");
+        request = protocolSerializer.array(List.of("PSYNC", "?", "-1"));
+        outputStream.write(request);
+        response = protocolDeserializer.parseInput(inputStream);
+        if (!response.startsWith("FULLRESYNC")) {
+            throw new RuntimeException("Unexpected response for PSYNC: " + response);
+        }
+
+        protocolDeserializer.parseRdbFile(inputStream);
+
+        System.out.println("Replica has started");
     }
 }
