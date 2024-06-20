adea3a5f5b7d4e89c167fad68affcc379b8a9873
Yevhenii Khaliuk <yevhenii.khaliuk@gmail.com>
2024-06-20 22:56:12 +0200

    The INCR command (3/3)

--- src/main/java/dev/khaliuk/ccredis/command/Incr.java
+++ src/main/java/dev/khaliuk/ccredis/command/Incr.java
@@ -4,6 +4,8 @@ import dev.khaliuk.ccredis.config.ObjectFactory;
 import dev.khaliuk.ccredis.storage.Storage;
 
 public class Incr extends AbstractHandler implements Write {
+    private static final String NOT_AN_INTEGER_ERROR_MESSAGE = "ERR value is not an integer or out of range";
+
     public Incr(ObjectFactory objectFactory) {
         super(objectFactory);
     }
@@ -11,8 +13,19 @@ public class Incr extends AbstractHandler implements Write {
     @Override
     public byte[] handle(String[] arguments) {
         var key = arguments[1];
-        var value = Storage.contains(key) ? Long.parseLong((String) Storage.get(key).value()) : 0;
-        value++;
+        long value;
+
+        if (Storage.contains(key)) {
+            try {
+                var storageValue = (String) Storage.get(key).value();
+                value = Long.parseLong(storageValue) + 1;
+            } catch (NumberFormatException e) {
+                return objectFactory.getProtocolSerializer().simpleError(NOT_AN_INTEGER_ERROR_MESSAGE);
+            }
+        } else {
+            value = 1;
+        }
+
         Storage.put(key, String.valueOf(value));
         return objectFactory.getProtocolSerializer().integer(value);
     }
